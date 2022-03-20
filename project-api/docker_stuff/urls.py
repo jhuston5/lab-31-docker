1affac02fc383e9c74eed57bb9b346ef879d3fc4
Joshua Huston <joshua.david.huston@gmail.com>
2022-03-19 20:35:46 -0400

    Completed Lab 32

--- project-api/docker_stuff/urls.py
+++ project-api/docker_stuff/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/dockerize/', include('dockerize.urls')),
+    path('api-auth', include('rest_framework.urls'))
 ]
